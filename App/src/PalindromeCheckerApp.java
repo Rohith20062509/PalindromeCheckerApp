0de17b1dc46e1f1d5b2e67048e762c5e519decd1
Rohith20062509 <sunny20062509@gmail.com>
2026-02-23 09:26:48 +0530

    intialcommit

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
+/*
+ *@author Rohith Reddy
+ *@Version 1.0
+ * This is to print Basic information
+ */
 
 public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("PalindromeCheckerApp");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully");
+    }
 }
